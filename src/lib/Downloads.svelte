3f5983263e8d5919a7aaed97b6583a4d0fce9ac5
Chris Chen <33755603+newageoflight@users.noreply.github.com>
2022-01-16 22:29:48 +1100

    Changes download links to pre-release

--- src/lib/Downloads.svelte
+++ src/lib/Downloads.svelte
@@ -8,11 +8,11 @@ import DownloadRow from "./components/DownloadRow.svelte";
     <p>Not yet available.</p>
     <h2>In parts</h2>
     <ol>
-        <li><DownloadRow link="#" status="public">Introduction to regional anatomy (50% complete)</DownloadRow></li>
+        <li><DownloadRow link="#" status="prerelease">Introduction to regional anatomy (50% complete)</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Upper limb</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Lower limb</DownloadRow></li>
-        <li><DownloadRow link="#" status="public">Thorax</DownloadRow></li>
-        <li><DownloadRow link="#" status="public">Abdomen (50% complete)</DownloadRow></li>
+        <li><DownloadRow link="#" status="prerelease">Thorax</DownloadRow></li>
+        <li><DownloadRow link="#" status="prerelease">Abdomen (50% complete)</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Head, neck and spine (61% complete)</DownloadRow></li>
         <li><DownloadRow link="#" status="nonrelease">CNS (57% complete)</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Skull osteology (100% complete)</DownloadRow></li>
