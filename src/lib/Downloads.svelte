ba45c0e9c22371ca6fc6b01bb5db81e4ed12b234
Chris Chen <33755603+newageoflight@users.noreply.github.com>
2022-02-12 23:47:07 +1100

    Update Downloads.svelte

--- src/lib/Downloads.svelte
+++ src/lib/Downloads.svelte
@@ -8,12 +8,12 @@ import DownloadRow from "./components/DownloadRow.svelte";
     <p>Not yet available.</p>
     <h2>In parts</h2>
     <ol>
-        <li><DownloadRow link="#" status="prerelease">Introduction to regional anatomy (50% complete)</DownloadRow></li>
+        <li><DownloadRow link="https://mega.nz/file/H0EB0IJD#ubi7nWBv2keN5Y1bv5OJ4kqqtW9BcxmJdaIbdPS-vq0" status="release">Introduction to regional anatomy (50% complete)</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Upper limb</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Lower limb</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Thorax</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Abdomen (50% complete)</DownloadRow></li>
-        <li><DownloadRow link="#" status="prerelease">Head, neck and spine (61% complete)</DownloadRow></li>
+        <li><DownloadRow link="#" status="nonrelease">Head, neck and spine (61% complete)</DownloadRow></li>
         <li><DownloadRow link="#" status="nonrelease">CNS (57% complete)</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Skull osteology</DownloadRow></li>
     </ol>
