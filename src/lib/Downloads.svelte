e8a273cf5e09b8e69f745f730c6f11205f20fe9c
Chris Chen <33755603+newageoflight@users.noreply.github.com>
2022-03-15 17:17:49 +1100

    Update Downloads.svelte

--- src/lib/Downloads.svelte
+++ src/lib/Downloads.svelte
@@ -13,7 +13,7 @@ import DownloadRow from "./components/DownloadRow.svelte";
         <li><DownloadRow link="#" status="prerelease">Upper limb</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Lower limb</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Thorax</DownloadRow></li>
-        <li><DownloadRow link="#" status="prerelease">Abdomen (50% complete)</DownloadRow></li>
+        <li><DownloadRow link="#" status="prerelease">Abdomen (95% complete)</DownloadRow></li>
         <li><DownloadRow link="#" status="nonrelease">Head, neck and spine (61% complete)</DownloadRow></li>
         <li><DownloadRow link="#" status="nonrelease">CNS (57% complete)</DownloadRow></li>
         <li><DownloadRow link="#" status="prerelease">Skull osteology</DownloadRow></li>
